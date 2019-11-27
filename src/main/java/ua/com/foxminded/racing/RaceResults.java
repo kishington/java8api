02482f0106bf0116a4e33aa5eee3c7b40c61a554
Mihail <yarovmk@gmail.com>
2019-11-27 07:11:58 +0000

    Renamed Qualification class to RaceResults

---
+++ src/main/java/ua/com/foxminded/racing/RaceResults.java
@@ -0,0 +1,12 @@
+package ua.com.foxminded.racing;
+
+import ua.com.foxminded.racing.data.Visualiser;
+
+public class RaceResults {
+
+    public static void main(String[] args) {
+        Visualiser visualiser = new Visualiser();
+        String results = visualiser.visualiseRaceResults();
+        System.out.println(results);
+    }
+}
