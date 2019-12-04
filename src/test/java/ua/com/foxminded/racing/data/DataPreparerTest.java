097b04e9caa6aaeb5fc5c884d008cd69f105a24a
Mihail <yarovmk@gmail.com>
2019-12-04 15:33:26 +0000

    Changed Assert.assertEquals to Assertions.assertEquals

--- src/test/java/ua/com/foxminded/racing/data/DataPreparerTest.java
+++ src/test/java/ua/com/foxminded/racing/data/DataPreparerTest.java
@@ -1,6 +1,6 @@
 package ua.com.foxminded.racing.data;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
